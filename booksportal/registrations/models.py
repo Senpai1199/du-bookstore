5427ee4f33e0ab4a2637ecd8cee9a5c5975824b2
Abhinav <abhinavtiwari824@gmail.com>
2020-05-12 17:27:35 +0530

    booksportal: added Course, Bookset model and altered Book model

--- booksportal/registrations/models.py
+++ booksportal/registrations/models.py
@@ -51,12 +51,14 @@ class Book(models.Model):
     description = models.CharField(max_length=200, blank=True)
     category = models.CharField(choices=BOOK_TYPES, null=False, max_length=1, default='S')
     condition = models.CharField(max_length=300, blank=False)
-    course = models.CharField(max_length=200, blank=True)
     year = models.SmallIntegerField(default=0, validators=[MinValueValidator(1), MaxValueValidator(4)]) # 4 for master
     semester = models.SmallIntegerField(default=0, validators=[MinValueValidator(1), MaxValueValidator(7)]) # 7 for masters
     image = models.ImageField(upload_to='book_pics', null=False)
+    course = models.ForeignKey('Course', related_name='course_books', on_delete=models.CASCADE)
     price = models.IntegerField(default=0)
     sold = models.BooleanField(default=False)
+    bookset = models.ForeignKey('BookSet', related_name='set_books', null=True,
+        blank=True, on_delete=models.CASCADE)
 
     def save(self, *args, **kwargs):
         super().save(*args, **kwargs)
@@ -85,3 +87,32 @@ class College(models.Model):
     def __str__(self):
         return "{} - {}".format(self.id, self.name)
 
+class Course(models.Model):
+    """
+        Handles Course data (to be populated using sheet).
+    """
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return "{} - {}".format(self.id, self.name)
+
+
+class BookSet(models.Model):
+    """
+        Used for selling a set of related books.
+        Contains Information about price, course, etc.
+    """
+    title = models.CharField(max_length=200, null=True)
+    description = models.CharField(max_length=200, blank=True)
+    course = models.ForeignKey('Course', related_name='course_bookset',
+        on_delete=models.CASCADE)
+    year = models.SmallIntegerField(default=0, validators=[MinValueValidator(1), MaxValueValidator(4)]) # 4 for master
+    semester = models.SmallIntegerField(default=0, validators=[MinValueValidator(1), MaxValueValidator(7)]) # 7 for masters
+    price = models.IntegerField(default=0)
+    sold = models.BooleanField(default=False)
+    contains_books = models.BooleanField(default=False)
+    contains_notes = models.BooleanField(default=False)
+    contains_readings = models.BooleanField(default=False)
+
+    def __str__(self):
+        return "{} - {}".format(self.id, self.title)
