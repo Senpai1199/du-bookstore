ceb50c0bd1a99d1d440eb4a30e4b827b98e4c19d
Divyansh Agarwal <divyansh1199@gmail.com>
2020-05-13 01:13:14 +0530

    Restored static/registrations folder

---
+++ booksportal/registrations/static/registrations/js/script.js
@@ -0,0 +1,23 @@
+﻿$(document).ready(function(){
+    $('.chk input').on('change',function(){
+        var value = $(this).parent('td').next('td').html();
+        value = parseInt(value);
+        if(this.checked == true) {
+            $("#amount").html( parseInt($("#amount").html()) + value );
+        }
+        else {
+            $("#amount").html( parseInt($("#amount").html()) - value );
+        }
+    });
+
+    $('#selectAll').click(function(e){
+        var table= $(e.target).closest('table');
+        $('td input:checkbox',table).prop('checked',this.checked);
+    });
+
+    $('#selectAll1').click(function(e){
+        var table= $(e.target).closest('table');
+        $('td input:checkbox',table).prop('checked',this.checked);
+    });
+});
+
