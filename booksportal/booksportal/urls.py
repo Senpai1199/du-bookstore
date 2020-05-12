91f5a2e86ff4ee8fd40c0afef0b8d90db121b86c
Divyansh Agarwal <divyansh1199@gmail.com>
2020-05-13 01:12:09 +0530

    Add contact and about view and template

--- booksportal/booksportal/urls.py
+++ booksportal/booksportal/urls.py
@@ -15,9 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 from registrations import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('base/', views.base_view, name='base'),
-]
+    path('about/', views.about, name='about'),
+    path('contact/', views.contact, name='contact'),
+
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- booksportal/registrations/views.py
+++ booksportal/registrations/views.py
@@ -3,3 +3,10 @@ from django.shortcuts import render
 # Create your views here.
 def base_view(request):
     return render(request,'registrations/base.html', context={})
+
+def about(request):
+    return render(request, 'registrations/about.html')
+
+def contact(request):
+    return render(request, 'registrations/contact.html')
+
