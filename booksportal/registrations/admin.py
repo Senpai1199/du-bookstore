e0974c4021c14880c619d339e93c167373c2df1e
Divyansh Agarwal <divyansh1199@gmail.com>
2020-05-13 23:56:51 +0530

    Registered models on admin

--- booksportal/registrations/admin.py
+++ booksportal/registrations/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-from registrations.models import Book, College, UserProfile
+from registrations.models import Book, College, UserProfile, Course, BookSet
 
 # Register your models here.
 
 admin.site.register(Book)
 admin.site.register(College)
 admin.site.register(UserProfile)
+admin.site.register(Course)
+admin.site.register(BookSet)
