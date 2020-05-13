1de4157bbfa62330b5c92b45e47764bdc3810a8a
Divyansh Agarwal <divyansh1199@gmail.com>
2020-05-13 14:16:41 +0530

    Add Dashboard widgets in base.html

--- booksportal/registrations/views.py
+++ booksportal/registrations/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def base_view(request):
-    return render(request,'registrations/base.html', context={})
+    return render(request,'registrations/base.html', context={"full_name": request.user.first_name + ' ' + request.user.last_name})
 
 def about(request):
     return render(request, 'registrations/about.html')
