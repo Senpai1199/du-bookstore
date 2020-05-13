6f84c8b7e5a05ed60e9df5f356359c334e976843
Divyansh Agarwal <divyansh1199@gmail.com>
2020-05-14 01:30:32 +0530

    Add view logic for home/, edit widgets in base.html

--- booksportal/registrations/views.py
+++ booksportal/registrations/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render, redirect
+from itertools import chain
+
+from registrations.models import Book
 
 
 def index(request):
@@ -14,8 +17,13 @@ def home(request):
     """
         The HOME page of the portal where all the book listings would be displayed according to the year of the logged in user
     """
+    books1 = Book.objects.filter(year=request.user.profile.year)
+    books2 = Book.objects.all().exclude(year=request.user.profile.year)
+    books = list(chain(books1, books2))
+    print("****" + str(len(books)) + "*****")
     context = {
-        "full_name": request.user.first_name + ' ' + request.user.last_name
+        "full_name": request.user.first_name + ' ' + request.user.last_name,
+        "books": books
     }
     return render(request, 'registrations/home.html')
 
