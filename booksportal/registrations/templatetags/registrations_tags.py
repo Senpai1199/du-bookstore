382e2b74b367ac6a9534e67debdb4cafbf887314
abhinav <abhinavtiwari824@gmail.com>
2020-05-14 13:17:17 +0530

    HomePage: added templatetags for rendering book year-sem, fixed some styling components

---
+++ booksportal/registrations/templatetags/registrations_tags.py
@@ -0,0 +1,25 @@
+from django import template
+
+register = template.Library()
+
+@register.simple_tag()
+def get_year_semester(book):
+
+    ordinal_indicator = {
+                            1 : 'st',
+                            2 : 'nd',
+                            3 : 'rd'
+                         }
+
+    year = book.year
+    sem = book.semester
+
+    if year < 4:
+        year_string = str(year) + ordinal_indicator[year]
+        semester_string = str(sem) + ordinal_indicator[sem]
+        year_sem_string = '<span class="label bg-cyan">{} year</span> \
+        <span class="label bg-cyan">{} semester</span>'.format(year_string, semester_string)
+    else:
+        year_sem_string = '<span class="label bg-cyan">Masters</span>'
+
+    return year_sem_string
